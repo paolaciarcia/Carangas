ad6a3dfe4ebb27dea74aebe76d09dc1bbd3eef6b
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-06-25 16:09:25 -0300

    add object Car

---
+++ Carangas/Car.swift
@@ -0,0 +1,29 @@
+//
+//  Car.swift
+//  Carangas
+//
+//  Created by Erick Borges on 25/06/21.
+//
+
+import Foundation
+
+struct Car: Codable {
+    let id: String
+    let brand: String
+    let gasType: Int
+    let name: String
+    let price: Double
+    
+    var gas: String {
+        
+        switch gasType {
+        case 0:
+           return "Flex"
+        case 1:
+           return "Álcool"
+        default:
+        return "Gasolina"
+          
+        }
+    }
+}
--- Carangas/CarsTableViewController.swift
+++ Carangas/CarsTableViewController.swift
@@ -20,25 +20,17 @@ class CarsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -84,5 +76,9 @@ class CarsTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
 }
